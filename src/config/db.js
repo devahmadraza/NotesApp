352ec6ff35408ccb6e36bff7bad99c6edd1df5f8
devahmadraza <devahmadraza296@gmail.com>
2026-09-04 22:58:18 +0500

    Notes App:Complete Routes &MongoDB connection  in process 52:34

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 export const connectDB=async()=>{
     try {
-       await mongoose.connect("mongodb+srv://devahmadraza296_db_user:rqQ9t99218MasWiP@cluster0.tfbt5ee.mongodb.net/?appName=Cluster0")
+       await mongoose.connect("")
 console.log('MongoDB Connected Successfully')
 } catch (error) {
         console.error('Error connecting to MongoDB',error)
--- src/server.js
+++ src/server.js
@@ -9,6 +9,3 @@ app.use("/api/notes" , notesRoutes)
 app.listen(5001,()=>{
 console.log("Server is started on PORT:  5001");
 })
-
-// rqQ9t99218MasWiP
-// devahmadraza296_db_user
